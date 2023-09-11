a6e0036daa7b7a95d0555eddaab9396ca067f8fd
Ömer <omergurbuz25@hotmail.com>
2023-09-11 23:19:03 +0300

    Tamamlandi

--- src/components/DateTimePicker.js
+++ src/components/DateTimePicker.js
@@ -2,80 +2,60 @@ import React, { useState } from "react";
 import "./DateTimePicker.css";
 
 const DateTimePicker = () => {
-  // İlk input için state
   const [firstDateTime, setFirstDateTime] = useState(new Date());
 
-  // İkinci input için saat state'i
   const [secondHours, setSecondHours] = useState("00");
 
-  // İkinci input için dakika state'i
   const [secondMinutes, setSecondMinutes] = useState("00");
 
-  // İkinci input için saniye state'i
   const [secondSeconds, setSecondSeconds] = useState("00");
 
-  // Sonucu saklayacak state
   const [result, setResult] = useState("");
 
-  // İlk inputun değişimini ele alacak fonksiyon
   const handleFirstDateTimeChange = (e) => {
     setFirstDateTime(e.target.value);
   };
 
-  // İkinci input için saat değişimini ele alacak fonksiyon
   const handleSecondHoursChange = (e) => {
     setSecondHours(e.target.value);
   };
 
-  // İkinci input için dakika değişimini ele alacak fonksiyon
   const handleSecondMinutesChange = (e) => {
     setSecondMinutes(e.target.value);
   };
 
-  // İkinci input için saniye değişimini ele alacak fonksiyon
   const handleSecondSecondsChange = (e) => {
     setSecondSeconds(e.target.value);
   };
 
-  // İki tarihi toplayarak sonucu hesaplayan fonksiyon
   const calculateAdditionResult = () => {
-    // İlk tarihi bir JavaScript Date nesnesine çevirin
     const firstDate = new Date(firstDateTime);
 
-    // İkinci tarihi alın
     const hours = parseInt(secondHours, 10);
     const minutes = parseInt(secondMinutes, 10);
     const seconds = parseInt(secondSeconds, 10);
 
-    // İlk tarihin saatini, dakikasını ve saniyesini güncelleyin
     firstDate.setHours(firstDate.getHours() + hours);
     firstDate.setMinutes(firstDate.getMinutes() + minutes);
     firstDate.setSeconds(firstDate.getSeconds() + seconds);
 
-    // Sonucu döndürün
     return firstDate.toLocaleString();
   };
 
-  // İki tarihi çıkararak sonucu hesaplayan fonksiyon
   const calculateSubtractionResult = () => {
-    // İlk tarihi bir JavaScript Date nesnesine çevirin
     const firstDate = new Date(firstDateTime);
 
-    // İkinci tarihi alın
     const hours = parseInt(secondHours, 10);
     const minutes = parseInt(secondMinutes, 10);
     const seconds = parseInt(secondSeconds, 10);
 
-    // İlk tarihin saatini, dakikasını ve saniyesini güncelleyin
     firstDate.setHours(firstDate.getHours() - hours);
     firstDate.setMinutes(firstDate.getMinutes() - minutes);
     firstDate.setSeconds(firstDate.getSeconds() - seconds);
 
-    // Sonucu döndürün
     return firstDate.toLocaleString();
   };
 
-  // Form submit edildiğinde sonuçları hesaplayan fonksiyon
   const handleSubmit = (e) => {
     e.preventDefault();
     const additionResult = calculateAdditionResult();
@@ -128,7 +108,9 @@ const DateTimePicker = () => {
           />
         </div>
 
-        <button type="submit">Hesapla</button>
+        <button class="button" type="submit">
+          Hesapla
+        </button>
       </form>
 
       {result && (
