293ea6fb5f7d700d4adae369bc5a0d9cb6fc1a4e
Ömer <omergurbuz25@hotmail.com>
2023-09-16 02:22:56 +0300

    Tamamlandi

--- src/components/DateTimePicker.js
+++ src/components/DateTimePicker.js
@@ -2,58 +2,112 @@ import React, { useState } from "react";
 import "./DateTimePicker.css";
 
 const DateTimePicker = () => {
-  const [firstDateTime, setFirstDateTime] = useState(new Date());
+  const [firstDate, setFirstDate] = useState("2023-09-11");
+  const [firstHour, setFirstHour] = useState("00");
+  const [firstMinute, setFirstMinute] = useState("00");
+  const [firstSecond, setFirstSecond] = useState("00");
 
   const [secondHours, setSecondHours] = useState("00");
-
   const [secondMinutes, setSecondMinutes] = useState("00");
-
   const [secondSeconds, setSecondSeconds] = useState("00");
 
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState({
+    additionResult: "",
+    subtractionResult: "",
+  });
 
-  const handleFirstDateTimeChange = (e) => {
-    setFirstDateTime(e.target.value);
+  const handleFirstDateChange = (e) => {
+    setFirstDate(e.target.value);
+  };
+
+  // Input değerlerini düzenleyen fonksiyon
+  const handleInputChange = (e, stateSetter) => {
+    const value = e.target.value;
+    if (value.length === 1 && /^\d$/.test(value)) {
+      stateSetter(`0${value}`);
+    } else if (value.length === 2 && /^\d\d$/.test(value)) {
+      stateSetter(value);
+    } else if (value.length === 3 && /^\d\d\d$/.test(value)) {
+      stateSetter(value.substring(1));
+    } else {
+      stateSetter("");
+    }
+  };
+
+  const handleFirstHourChange = (e) => {
+    handleInputChange(e, setFirstHour);
+  };
+
+  const handleFirstMinuteChange = (e) => {
+    handleInputChange(e, setFirstMinute);
+  };
+
+  const handleFirstSecondChange = (e) => {
+    handleInputChange(e, setFirstSecond);
   };
 
   const handleSecondHoursChange = (e) => {
-    setSecondHours(e.target.value);
+    handleInputChange(e, setSecondHours);
   };
 
   const handleSecondMinutesChange = (e) => {
-    setSecondMinutes(e.target.value);
+    handleInputChange(e, setSecondMinutes);
   };
 
   const handleSecondSecondsChange = (e) => {
-    setSecondSeconds(e.target.value);
+    handleInputChange(e, setSecondSeconds);
   };
 
   const calculateAdditionResult = () => {
-    const firstDate = new Date(firstDateTime);
-
+    const firstDateTime = new Date(
+      `${firstDate}T${firstHour}:${firstMinute}:${firstSecond}`
+    );
     const hours = parseInt(secondHours, 10);
     const minutes = parseInt(secondMinutes, 10);
     const seconds = parseInt(secondSeconds, 10);
 
-    firstDate.setHours(firstDate.getHours() + hours);
-    firstDate.setMinutes(firstDate.getMinutes() + minutes);
-    firstDate.setSeconds(firstDate.getSeconds() + seconds);
+    if (!isNaN(hours) && !isNaN(minutes) && !isNaN(seconds)) {
+      firstDateTime.setHours(firstDateTime.getHours() + hours);
+      firstDateTime.setMinutes(firstDateTime.getMinutes() + minutes);
+      firstDateTime.setSeconds(firstDateTime.getSeconds() + seconds);
 
-    return firstDate.toLocaleString();
+      const year = firstDateTime.getFullYear();
+      const month = String(firstDateTime.getMonth() + 1).padStart(2, "0");
+      const day = String(firstDateTime.getDate()).padStart(2, "0");
+      const hoursStr = String(firstDateTime.getHours()).padStart(2, "0");
+      const minutesStr = String(firstDateTime.getMinutes()).padStart(2, "0");
+      const secondsStr = String(firstDateTime.getSeconds()).padStart(2, "0");
+
+      return `${day}-${month}-${year} ${hoursStr}:${minutesStr}:${secondsStr}`;
+    } else {
+      return "";
+    }
   };
 
   const calculateSubtractionResult = () => {
-    const firstDate = new Date(firstDateTime);
-
+    const firstDateTime = new Date(
+      `${firstDate}T${firstHour}:${firstMinute}:${firstSecond}`
+    );
     const hours = parseInt(secondHours, 10);
     const minutes = parseInt(secondMinutes, 10);
     const seconds = parseInt(secondSeconds, 10);
 
-    firstDate.setHours(firstDate.getHours() - hours);
-    firstDate.setMinutes(firstDate.getMinutes() - minutes);
-    firstDate.setSeconds(firstDate.getSeconds() - seconds);
+    if (!isNaN(hours) && !isNaN(minutes) && !isNaN(seconds)) {
+      firstDateTime.setHours(firstDateTime.getHours() - hours);
+      firstDateTime.setMinutes(firstDateTime.getMinutes() - minutes);
+      firstDateTime.setSeconds(firstDateTime.getSeconds() - seconds);
 
-    return firstDate.toLocaleString();
+      const year = firstDateTime.getFullYear();
+      const month = String(firstDateTime.getMonth() + 1).padStart(2, "0");
+      const day = String(firstDateTime.getDate()).padStart(2, "0");
+      const hoursStr = String(firstDateTime.getHours()).padStart(2, "0");
+      const minutesStr = String(firstDateTime.getMinutes()).padStart(2, "0");
+      const secondsStr = String(firstDateTime.getSeconds()).padStart(2, "0");
+
+      return `${day}-${month}-${year} ${hoursStr}:${minutesStr}:${secondsStr}`;
+    } else {
+      return "";
+    }
   };
 
   const handleSubmit = (e) => {
@@ -66,18 +120,51 @@ const DateTimePicker = () => {
   return (
     <div className="date-time-picker">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="first-datetime">İlk Tarih ve Saat Seçin: </label>
-        <input
-          type="datetime-local"
-          id="first-datetime"
-          name="first-datetime"
-          value={firstDateTime}
-          onChange={handleFirstDateTimeChange}
-          step="1"
-        />
-
-        <label htmlFor="second-time">Saat, Dakika ve Saniye Seçin: </label>
-        <div className="second-time-inputs">
+        <div className="datetime-input">
+          <label htmlFor="first-date">Tarih Seçin: </label>
+          <input
+            type="date"
+            id="first-date"
+            name="first-date"
+            value={firstDate}
+            onChange={handleFirstDateChange}
+          />
+          <label htmlFor="first-hour">Saat Seçin (Saat-Dakika-Saniye): </label>
+          <div className="first-time">
+            {" "}
+            <input
+              type="number"
+              id="first-hour"
+              name="first-hour"
+              value={firstHour}
+              onChange={handleFirstHourChange}
+              min="0"
+              max="23"
+            />
+            <span> : </span>
+            <input
+              type="number"
+              id="first-minute"
+              name="first-minute"
+              value={firstMinute}
+              onChange={handleFirstMinuteChange}
+              min="0"
+              max="59"
+            />{" "}
+            <span> : </span>
+            <input
+              type="number"
+              id="first-second"
+              name="first-second"
+              value={firstSecond}
+              onChange={handleFirstSecondChange}
+              min="0"
+              max="59"
+            />
+          </div>
+        </div>
+        <label htmlFor="second-hours">İkinci Saat: </label>
+        <div className="time-input">
           <input
             type="number"
             id="second-hours"
@@ -85,6 +172,7 @@ const DateTimePicker = () => {
             value={secondHours}
             onChange={handleSecondHoursChange}
             min="0"
+            max="23"
           />
           <span> : </span>
           <input
@@ -107,14 +195,12 @@ const DateTimePicker = () => {
             max="59"
           />
         </div>
-
-        <button class="button" type="submit">
-          Hesapla
+        <button class="ui-btn">
+          <span>Hesapla</span>
         </button>
       </form>
-
       {result && (
-        <div>
+        <div className="result">
           <p>Toplama Sonucu: {result.additionResult}</p>
           <p>Çıkarma Sonucu: {result.subtractionResult}</p>
         </div>
